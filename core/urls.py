e938ce012d7ca0554ab26a29aa5039e16cd65474
Akenore <joudakenore@gmail.com>
2026-04-02 23:36:47 +0100

    feat: implement custom media file serving to support production environments without Nginx

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 admin_url = f"{settings.DJANGO_ADMIN_URL.strip('/')}/"
 
@@ -14,6 +15,11 @@ urlpatterns = [
     path("", include("shared.urls")),
 ]
 
-
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Always serve media files (even in production) since Coolify doesn't run Nginx locally
+urlpatterns += [
+    re_path(rf"^{settings.MEDIA_URL.lstrip('/')}(?P<path>.*)$", serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
